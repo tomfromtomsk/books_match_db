2acb0eb59b86533a29e5205dcb3d5944e276e266
Артём Вершинин <vershinintmsk@yandex.ru>
2024-05-14 11:32:34 +0300

    update files

--- db_scripts/db_ddl.sql
+++ db_scripts/db_ddl.sql
@@ -3,11 +3,12 @@
 DROP TABLE IF EXISTS books;
 
 CREATE TABLE books (
-   book_id 		int PRIMARY KEY
-  ,title 		varchar(255) NOT NULL
-  ,author 		varchar(255) NOT NULL
-  ,genre 		varchar(255)
-  ,year 		int
+   book_id 		  int PRIMARY KEY
+  ,title 		    varchar(255) NOT NULL
+  ,author 		  varchar(255) NOT NULL
+  ,genre 		    varchar(255)
+  ,description  varchar(255)
+  ,year 		    int
 );
 
 DROP TABLE IF EXISTS users;
@@ -15,19 +16,18 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
    user_id 		int PRIMARY KEY
   ,username 	varchar(255) NOT NULL
-  ,email 		varchar(255) NOT NULL UNIQUE
-  ,password		varchar(255) NOT NULL
+  ,email 		  varchar(255) NOT NULL UNIQUE
 );
 
 DROP TABLE IF EXISTS meetings;
 
 CREATE TABLE meetings (
    meeting_id 	int PRIMARY KEY
-  ,book_id 		int NOT NULL
-  ,user_id 		int NOT NULL
-  ,rating 		int CHECK (rating BETWEEN 1 AND 10)
-  ,comment 		text
-  ,date 		date DEFAULT NOW
+  ,book_id 		  int NOT NULL
+  ,user_id 		  int NOT NULL
+  ,rating 		  int CHECK (rating BETWEEN 1 AND 10)
+  ,comment 		  text
+  ,date 		    date DEFAULT NOW()
   ,FOREIGN KEY (book_id) REFERENCES books (book_id)
   ,FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
